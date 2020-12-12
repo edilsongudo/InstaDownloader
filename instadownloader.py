284f2ea9de109f9a7bad48d0a1e3fdec795c02b2
CristianGrey <60367083+CristianGrey@users.noreply.github.com>
2020-12-12 22:33:32 +0200

    Update instadownloader.py
    The issue with the former video download code not working, was that Instagram now wants the user-agent. Solved that error adding user agent to the script

--- instadownloader.py
+++ instadownloader.py
@@ -15,7 +15,7 @@ def long_operation_thread():
 
     HOME = 'C:/Program Files/InstagramDownloader'
 
-    L = instaloader.Instaloader(download_pictures=False, download_video_thumbnails=False, download_geotags=False, download_comments=False, compress_json=False)
+    L = instaloader.Instaloader()
 
     if os.path.isdir(HOME) == False:
         os.mkdir(HOME)
@@ -83,6 +83,10 @@ def long_operation_thread():
         with open(os.path.join(HOME, f'Config/SLEEP_SECONDS_PER_DOWNLOAD.txt'), 'w') as f:
             f.write('5')
 
+    if os.path.isfile(os.path.join(HOME, f'Config/USER_AGENT.txt')) == False:
+        with open(os.path.join(HOME, f'Config/USER_AGENT.txt'), 'w') as f:
+            f.write('Mozilla/5.0 (Windows NT 6.1; rv:79.0) Gecko/20100101 Firefox/79.0')
+
     with open(os.path.join(HOME, f'Config/usernames.txt'), 'r') as f:
         usernames = f.readlines()
 
@@ -99,10 +103,7 @@ def long_operation_thread():
     with open(os.path.join(HOME, f'Config/SLEEP_SECONDS_PER_DOWNLOAD.txt'), 'r') as f:
         SLEEPTIME = int(f.read().strip())
 
-    print(f'Sleep time betwwen downloads is set to {SLEEPTIME} seconds.')
-
-    with open(os.path.join(HOME, f'Config/ALLOW_VIDEO.txt'), 'r') as f:
-        ALLOW_VIDEO = f.read().lower().strip()
+    print(f'Sleep time betwwen downloads is set to {SLEEPTIME}.')
 
     with open(os.path.join(HOME, f'Config/ONLY_VIDEOS.txt'), 'r') as f:
         ONLY_VIDEOS = f.read().lower().strip()
@@ -112,7 +113,15 @@ def long_operation_thread():
     else:
         print(f'Only videos is set to {ONLY_VIDEOS}. You can set only "true" or "false".')
 
+    with open(os.path.join(HOME, f'Config/ALLOW_VIDEO.txt'), 'r') as f:
+        ALLOW_VIDEO = f.read().lower().strip()
+
     print(f'Allow video is set to {ALLOW_VIDEO}. You can set only "true" or "false".')
+
+    with open(os.path.join(HOME, f'Config/USER_AGENT.txt'), 'r') as f:
+        USER_AGENT = f.read().strip()
+
+    print(f'User agent: {USER_AGENT}')
 
     with open(os.path.join(HOME, f'Login/account_username.txt'), 'r') as f:
         USERNAME = f.read().lower().strip()
@@ -159,11 +168,50 @@ def long_operation_thread():
                     
                     if post.is_video == True:
                         if ALLOW_VIDEO != 'false':
-                            try:
-                                os.chdir(os.path.join(HOME,'Posts'))
-                            except:
-                                pass
-                            L.download_post(post, username)
+                            ext = '.mp4'
+                            filename = f'{post.owner_username}_{post_date}{ext}'
+                            if os.path.isfile(os.path.join(HOME, f'Posts/{filename}')) == False:    
+                                url = f'https://www.instagram.com/p/{post.shortcode}'
+                                x = re.match(r'^(https:)[/][/]www.([^/]+[.])*instagram.com', url)
+
+                                try:
+                                    if x:
+                                        headers = {'User-Agent': USER_AGENT}
+                                        request_image = requests.get(url, headers=headers)
+                                        src = request_image.content.decode('utf-8')
+                                        check_type = re.search(r'<meta name="medium" content=[\'"]?([^\'" >]+)', src)
+                                        check_type_f = check_type.group()
+                                        final = re.sub('<meta name="medium" content="', '', check_type_f)
+
+                                        if final == "video": 
+                                            msg = 'yes'
+
+                                            if msg == "yes":
+                                                print("Downloading the video...")
+                                                extract_video_link = re.search(r'meta property="og:video" content=[\'"]?([^\'" >]+)', src)
+                                                video_link = extract_video_link.group()
+                                                final = re.sub('meta property="og:video" content="', '', video_link)
+                                                _response = requests.get(final, headers=headers).content
+                                                file_size_request = requests.get(final, stream=True)
+                                                file_size = int(file_size_request.headers['Content-Length'])
+                                                block_size = 1024 
+                                                # filename = f'{post.owner_username}_{post_date}{ext}'
+                                                # while os.path.isfile(os.path.join(HOME, f'Posts/{filename}')) == True:
+                                                #     filename = f'{post.owner_username}_{post_date}({str(z)}){ext}'
+                                                #     z += 1
+                                                t=tqdm(total=file_size, unit='B', unit_scale=True, desc=filename, ascii=True)
+                                                with open(f'{os.path.join(HOME,"Posts/")}{filename}', 'wb') as f:
+                                                    for data in file_size_request.iter_content(block_size):
+                                                        # t.update(len(data))
+                                                        f.write(data)
+                                                t.close()
+                                                print("Video downloaded successfully")
+                                                n += 1
+                                                sleep(SLEEPTIME)
+                                except Exception as e:
+                                    print(e)
+                            else:
+                                print(f'{filename} already downloaded')
                         else:
                             print('ALLOW_VIDEO.txt is set to "false". Set it to "true" to enable it')
 
@@ -183,9 +231,7 @@ def long_operation_thread():
                                     n += 1
                                     sleep(SLEEPTIME)
                             else:
-                                print(f'{filename} already downloaded.')
-                        else:
-                            pass                    
+                                print(f'{filename} already downloaded.')                    
                 else:
                     pass
 
@@ -193,6 +239,7 @@ def long_operation_thread():
             print(e)
           
     print('Finished')
+
                     
 def the_gui():
 
